0b09c53469e9fb89312d4705c686f21770aed6cf
Johnyblondynek <maxbroll.com@gmail.com>
2022-05-10 14:06:44 +0200

    Program

---
+++ Automat/Automat/Automat.cpp
@@ -0,0 +1,53 @@
+﻿#include <iostream>
+#include <windows.h>
+using namespace std;
+int main()
+{
+    int Wyb = 0;
+    bool Program=true;
+    int Reszta, P, i;
+    int nominaly[]{ 500,200,100,50,10,5,2,1 };
+    while (Program)
+    {
+        cout << "Jestem Bankomat :/"<<endl;
+        cout << "1:Rozmień"<<endl;
+        cout << "2:Autorzy"<<endl;
+        cout << "3:wyjdz"<<endl;
+        cin >> Wyb;
+        if (Wyb == 1) {
+            cout << "Podaj reszte do wyplacenia: ";
+            cin >> Reszta;
+            i = 0;
+            while (Reszta > 0)
+            {
+                if (Reszta >= nominaly[i])
+                {
+                    P = Reszta / nominaly[i];
+                    Reszta = Reszta - (nominaly[i] * P);
+                    cout << nominaly[i] << " x " << P << endl;
+                }
+                i++;
+            }
+                Sleep(2000);
+                cout << "Wpisz dowolną liczbę aby przejśc od menu" << endl;
+
+                cin >> Wyb;
+                system("cls");
+        }
+        else if (Wyb == 2) {
+            system("cls");
+            cout << "Maxwell Broll" << endl;
+            cout << "Luka Stojanović" << endl;
+            Sleep(2000);
+            system("cls");
+        }
+        else if (Wyb == 3) {
+            Program = false;
+        }
+        else {
+            system("cls");
+            cout << "Błąd" << endl;
+            Sleep(2000);
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Bankomat-wydawanie-reszty
+Program wydaję resztę kiedy użytkownik wprowadza wartość wydawania reszty
+Autorzy:Maxwell Broll i Luka Stojanović
